6df08a5cdc1a630f62c864153fe727e8a20032be
Caroline Old <carolinejold@gmail.com>
2020-10-11 02:07:00 +0200

    functionality working

--- src/components/ExtraCard.tsx
+++ src/components/ExtraCard.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
 
-interface ExtraCardProps {}
+interface ExtraCardProps {
+  tempData: {
+    temp?: number;
+    feels_like?: number;
+    humidity?: number;
+    temp_max?: number;
+    temp_min?: number;
+  };
+}
 
-export const ExtraCard: React.FC<ExtraCardProps> = ({}) => {
-    return <div>Extra card</div>;
+export const ExtraCard: React.FC<ExtraCardProps> = ({ tempData }) => {
+  return (
+    <div>
+        <div>
+          Feels like: {tempData.feels_like}°C
+          <br></br>
+          Humidity: {tempData.humidity}
+          <br></br>
+          Min temperature: {tempData.temp_min}°C
+          <br></br>
+          Max temperature: {tempData.temp_max}°C
+        </div>
+    </div>
+  );
 };
--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import { ExtraCard } from "./ExtraCard";
 
-interface MainCardProps {}
+interface MainCardProps {
+  userInputData: string[];
+  tempData: {
+    temp?: number;
+    feels_like?: number;
+    humidity?: number;
+    temp_max?: number;
+    temp_min?: number;
+  };
+  weatherData: {
+    id?: number;
+    main?: string;
+    description?: string;
+    icon?: string;
+  };
+}
 
-export const MainCard: React.FC<MainCardProps> = ({}) => {
-    return <div>Main card</div>;
+export const MainCard: React.FC<MainCardProps> = ({
+  userInputData,
+  weatherData,
+  tempData,
+}) => {
+  const [toggleExtra, setToggleExtra] = useState<boolean | any>(false);
+
+  const handleClick = () => {
+    setToggleExtra(!toggleExtra);
+    console.log(toggleExtra);
+  };
+
+  return (
+    <div
+      style={{
+        visibility: userInputData[0] === undefined ? "hidden" : "visible",
+      }}
+    >
+      <div>
+        {userInputData}
+        <br></br>
+        <img
+          src={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+          alt="weather icon"
+        ></img>
+        <br></br>
+        It is currently {tempData.temp}°C with{" "}
+        {weatherData.description === "few clouds"
+          ? `a ${weatherData.description}`
+          : weatherData.description}
+        <br></br>
+      </div>
+      <button onClick={handleClick}>More details...</button>
+      <div
+        style={{
+          visibility: toggleExtra === true ? "visible" : "hidden",
+        }}
+      >
+        <ExtraCard tempData={tempData} />
+      </div>
+    </div>
+  );
 };
